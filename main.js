ad3a0ae5782499ee3603cb0f736e30b45c615c58
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-14 17:41:27 -0500

    Added necessary variables

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ let search = document.querySelector('.searchForm');
 
 search.addEventListener('submit', function(event) {
   event.preventDefault();
+  let textBox = document.querySelector('.textBox');
+  let searchInput = textBox.value;
+  let results = document.querySelector('.results');
+  let url = 'https://itunes.apple.com/search?term=';
+  let sectResults = document.querySelector('.results');
+  let trackResult = '';
+  let audio = document.querySelector('audio');
 });
